064854558876a296c4828e8f7c7e90185766f70b
DavidB <dvd.bajus@gmail.com>
2023-01-12 09:24:39 +0100

    changes after prepare script

--- config/provider.go
+++ config/provider.go
@@ -10,12 +10,12 @@ import (
 
 	ujconfig "github.com/upbound/upjet/pkg/config"
 
-	"github.com/upbound/upjet-provider-template/config/null"
+	"github.com/DavidBajus/provider-github-example/config/null"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/upbound/upjet-provider-template"
+	resourcePrefix = "github-example"
+	modulePath     = "github.com/DavidBajus/provider-github-example"
 )
 
 //go:embed schema.json
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/upbound/upjet-provider-template
+module github.com/DavidBajus/provider-github-example
 
 go 1.19
 
